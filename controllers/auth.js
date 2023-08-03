483e044f1c50cff0c0337a67f35e8cf0e202308b
Karan <risingphoenix160@gmail.com>
2023-08-03 23:07:00 +0530

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const jwt = require('jsonwebtoken');
+
+const register = async(req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createToken();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+}
+
+const login = async(req, res) => {
+  res.send('login user!');
+}
+
+module.exports = {
+  register,
+  login
+}
