d808d329f617c6af6231c815ea57ab3c492c80da
Karan <risingphoenix160@gmail.com>
2023-08-04 15:17:28 +0530

    Add authentication and job routers

--- models/User.js
+++ models/User.js
@@ -36,4 +36,9 @@ UserSchema.methods.createToken = function () {
   return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
 }
 
+UserSchema.methods.comparePassword = async function (candidatePasswd) {
+  const isMatch = await bcrypt.compare(candidatePasswd, this.password);
+  return isMatch;
+}
+
 module.exports = mongoose.model('User', UserSchema);
